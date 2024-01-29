b5c3b999d41bf2d077cf29ea614f0c2d9bc89c7c
yassine raddaoui <yassineraddaouicr7@gmail.com>
2024-01-29 20:32:24 +0100

    feat(cors disable)

--- src/main/java/com/example/youtubeclonebackend/Config/ApplicationConfig.java
+++ src/main/java/com/example/youtubeclonebackend/Config/ApplicationConfig.java
@@ -8,12 +8,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final UserRepository userRepository;
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                ;
+            }
+        };
+    }
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmailAddress(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
--- src/main/java/com/example/youtubeclonebackend/Config/SecurityConfiguration.java
+++ src/main/java/com/example/youtubeclonebackend/Config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.youtubeclonebackend.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
@@ -20,6 +21,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(r -> r
                         .requestMatchers(WHITE_LIST_URI)
